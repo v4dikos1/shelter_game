64d74417574450b10e03a2935333833b4aa727d3
Vadim Iskhakov <72247526+v4dikos1@users.noreply.github.com>
2025-04-26 19:00:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ API_TOKEN = os.getenv("API_TOKEN")
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
+
+CONFIG_FILE = os.getenv("CONFIG_FILE", "config.json")
